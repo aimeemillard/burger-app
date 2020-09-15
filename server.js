61df40ae3739d53bbfaa19e32f6d8723a4601d69
aimeemillard <53044587+aimeemillard@users.noreply.github.com>
2020-09-15 01:17:21 -0400

    update

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+app.use(express.static("views/images"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//TODO: Define your routes
+// Import routes and give the server access to them.
+var routes = require("../controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Listening on port:%s", PORT);
+});
